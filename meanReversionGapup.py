1fc3bbd3bd0692ea66247f5435ccc7b8638bf603
Tushar Wasake <tusharwasake@gmail.com>
2024-09-03 20:18:56 +0530

    Initial commit: Added Streamlit app and requirements.txt

---
+++ meanReversionGapup.py
@@ -0,0 +1,42 @@
+import pandas as pd 
+import numpy as np
+from pymongo import MongoClient
+import matplotlib.pyplot as plt
+import streamlit as st
+
+# Title of the app
+st.title("Gapup Strategy")
+
+# Connect to MongoDB
+mongo = MongoClient("mongodb+srv://my_algo_user:uN3VJWO5WDPYhYA9@cluster0.otr8r.mongodb.net/?retryWrites=true&w=majority")
+mydb = mongo['Test1']
+coll = mydb["Pnl_Gap"]
+
+# Fetch data from MongoDB
+table = list(coll.find(
+    {}, {'_id': 0, 'Date': 1, 'PNL': 1}))
+
+# Extract dates and PNLs
+date = [item['Date'] for item in table]
+pnl = [item['PNL'] for item in table]
+
+# Calculate cumulative PNL
+Sum_pnl = np.cumsum(pnl)
+
+# Create a DataFrame for the chart
+chart_data = pd.DataFrame({
+    'Date': date,
+    'Cumulative PNL': Sum_pnl
+})
+
+# Set the date as the index for better plotting
+chart_data.set_index('Date', inplace=True)
+
+# Plot the cumulative PNL
+st.line_chart(chart_data)
+
+# Display the table of data
+st.table(table)
+
+# Display the total PNL
+st.write("### Total PNL: ", Sum_pnl[-1])
